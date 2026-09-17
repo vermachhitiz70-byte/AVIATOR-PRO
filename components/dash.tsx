bbe75c3884970c037c656ed7633553235d73c57a
kjassi435 <82087340+kjassi435@users.noreply.github.com>
2026-09-17 17:19:22 +0530

    fix: missing useRouter import in dash components

--- components/dash.tsx
+++ components/dash.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FEED_NAMES } from "@/lib/config";
 import {
